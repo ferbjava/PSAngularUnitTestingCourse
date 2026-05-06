2b558277660b2d946b247f956c876b6a411c6566
Michal Kotecki <michal.kotecki@capgemini.com>
2026-05-06 11:25:47 +0200

    mod_06 test for the parent-child components with direct clicking

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -49,4 +49,17 @@ describe('HeroesComponent (deep tests)', () => {
       expect(heroComponentsDEs[i].componentInstance.hero).toEqual(HEROES[i]);
     }
   });
+
+  it(`should call heroService.deleteHero when the Hero Component's 
+    delete button is clicked`, () => {
+    spyOn(fixture.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    // run ngOnInit()
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    heroComponents[0].query(By.css('button')).triggerEventHandler('focus', {stopPropagation: () => {}});
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+    });
 });
